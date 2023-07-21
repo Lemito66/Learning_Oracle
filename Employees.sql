f1d5d4fd75d90de646b0e2238abd87317ca8bac2
Emill <lemito66@gmail.com>
2023-07-21 16:09:25 -0500

    secuencia y usar tabla employees

--- Employees.sql
+++ Employees.sql
@@ -1,5 +1,12 @@
+CREATE SEQUENCE sec_employees
+    INCREMENT BY 1
+    START WITH 1
+    MINVALUE 1
+    MAXVALUE 999999999999
+
+
 CREATE TABLE `employees` (
-  `employee_id` integer PRIMARY KEY,
+  `employee_id` number DEFAULT sec_employees.nextval PRIMARY KEY,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `email` varchar(100) UNIQUE NOT NULL,
@@ -7,8 +14,9 @@ CREATE TABLE `employees` (
 );
 
 
-CREATE SEQUENCE sec_employees
-    INCREMENT BY 1
-    START WITH 1
-    MINVALUE 1
-    MAXVALUE 999999999999
+
+
+
+-- Insertar valores
+
+INSERT INTO employees (employee_id, first_name, last_name, email, salary) VALUES (nextval('sec_employees'), 'John', 'Doe', 'lemito66@gmail.com', 1000);

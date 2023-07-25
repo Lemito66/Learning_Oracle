b482772d4bef39fc6ecbf696c4b93c043d0cbb1e
Emill <lemito66@gmail.com>
2023-07-24 19:24:11 -0500

    practica 3

---
+++ Introduction_to_Oracle_SQL/Volume_1/Practice_3/practice_3.sql
@@ -0,0 +1,17 @@
+-- Usando expressiones condicionales con Case
+select email from employees
+
+select case 
+       email when 'lemito10@gmail.com' then 'Este es el email de lemito66'
+       else 'No es el email de lemito66'
+       end as "Email de lemito66"  
+ from employees
+
+--Sintaxis de Case
+Case
+When condition1 Then result1
+When condition2 Then result2
+When condition3 Then result3
+Else result4
+End
+

888492c1290a501ec3064d17ba93e630ba1d7ec4
Emill <lemito66@gmail.com>
2023-07-25 13:07:16 -0500

    consulta a las tablas

---
+++ consultas_tabla_hr.sql
@@ -0,0 +1,9 @@
+select * from employees;
+
+select * from  regions;
+
+select * from countries;
+select * from locations;
+select * from departments;
+select * from jobs;
+select * from job_history;

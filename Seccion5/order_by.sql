0d3bdcacaca4017537ed3aa6543729489d99f115
Emill <lemito66@gmail.com>
2023-07-27 09:57:19 -0500

    order by

--- Seccion5/order_by.sql
+++ Seccion5/order_by.sql
@@ -2,3 +2,5 @@ select * from employees where department_id = 90
 order by salary desc
 
 select * from employees order by first_name
+
+select first_name, email from employees order by 2
---
+++ Seccion5/order_by.~sql
@@ -0,0 +1,4 @@
+select * from employees where department_id = 90
+order by salary desc
+
+select * from employees order by first_name

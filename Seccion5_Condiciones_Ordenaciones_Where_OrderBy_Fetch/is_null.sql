002f64b0eb1807400debf43dd11a6f4621986196
Emill <lemito66@gmail.com>
2023-07-28 09:54:41 -0500

    is null

--- Seccion5_Condiciones_Ordenaciones_Where_OrderBy_Fetch/is_null.sql
+++ Seccion5_Condiciones_Ordenaciones_Where_OrderBy_Fetch/is_null.sql
@@ -1,1 +1,5 @@
 select * from employees where commission_pct = null -- Comparar un nulo con otro nulo no tiene sentido
+
+select * from employees where commission_pct is null -- Solo para ver los nulos
+
+select * from employees where commission_pct is not null -- Los que no son nulos

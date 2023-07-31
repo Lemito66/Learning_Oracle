5808a66624ba6f367d79abbc6e7ac0b4d66fe167
Emill <lemito66@gmail.com>
2023-07-31 16:17:19 -0500

    fechas a caracter

---
+++ Seccion7_Funciones_de_Conversion/fechas_a_caracter.sql
@@ -0,0 +1,37 @@
+﻿-- to_char(date/number, 'formato')
+
+
+--to_char(date, 'formato')
+
+
+/*
+
+YYYY A�o 4 n�meros      
+YEAR A�o (ingles)
+MM Mes 2 digitos
+Month Mes en Texto
+Mon  Abreviatura del d�a
+Day D�a en texto
+DD N�mero del d�a
+
+*/
+
+select sysdate, to_char(sysdate, 'YYYY') from dual
+select sysdate, to_char(sysdate, 'Month') from dual
+select sysdate, to_char(sysdate, 'YYYY') from dual
+
+/*
+AM
+PM
+HH formato 12 horas
+HH24 formato 24 horas
+MI minuto
+SS segundos
+*/
+
+select to_char(sysdate, 'dd/mm/yyyy') from dual
+
+select sysdate, to_char(sysdate, 'hh') from dual
+select sysdate, to_char(sysdate, 'hh24') from dual
+select sysdate, to_char(sysdate, 'mi') from dual
+select sysdate, to_char(sysdate, '"Son las" HH24:MI "del día de hoy del año" YYYY') from dual

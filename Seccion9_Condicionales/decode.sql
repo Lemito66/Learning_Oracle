df4d752047bfe3b53cd4f4bc594a1c1344bb03f7
Emill <lemito66@gmail.com>
2023-08-14 15:44:22 -0500

    decode

---
+++ Seccion9_Condicionales/decode.sql
@@ -0,0 +1,27 @@
+--decode(valor, resultado, valor, resultado, sinosecumplenada)
+select
+    first_name,
+    department_id
+from
+    employees
+select
+    first_name,
+    department_id,
+    decode (
+        department_id,
+        10,
+        'Administration',
+        20,
+        'Marketing',
+        30,
+        'Purchasing',
+        40,
+        'Human Resources',
+        50,
+        'Shipping',
+        60,
+        'IT',
+        'Other'
+    ) as department_name
+from
+    employees

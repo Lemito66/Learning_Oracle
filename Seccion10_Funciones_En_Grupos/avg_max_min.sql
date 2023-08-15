24795a5105a23b49cd35a5d1228049975255011c
Emill <lemito66@gmail.com>
2023-08-15 15:22:35 -0500

    avg_max_min

---
+++ Seccion10_Funciones_En_Grupos/avg_max_min.sql
@@ -0,0 +1,19 @@
+-- Promedio
+select
+    avg(salary)
+from
+    employees;
+
+
+-- Maximo valor
+select
+    max(salary)
+from
+    employees;
+
+-- Minimo valor
+select
+    min(salary)
+from
+    employees;
+

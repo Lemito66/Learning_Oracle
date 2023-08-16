c5cf77525a3a57b63662e7c9210fb2206e9ae8c3
Emill <lemito66@gmail.com>
2023-08-16 17:07:17 -0500

    group by

--- Seccion10_Funciones_En_Grupos/group_by.sql
+++ Seccion10_Funciones_En_Grupos/group_by.sql
@@ -1,4 +1,9 @@
--- groupby permite agrupar por una columna y aplicar una funciÃ³n de agregaciÃ³n a las demÃ¡s columnas
+-- groupby permite agrupar por una columna y aplicar una función de agregación a las demás columnas
 
 select department_id, count(*) from employees
 group by department_id
+order by department_id
+
+select department_id, job_id, count(*) from employees
+group by department_id, job_id
+order by department_id

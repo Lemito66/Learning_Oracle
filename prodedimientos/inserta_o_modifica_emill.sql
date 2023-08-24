673c113f1c9ba0823cd58305c5245dd75e4fef08
Emill <lemito66@gmail.com>
2023-08-24 14:45:51 -0500

    inserta o modifica

--- prodedimientos/inserta_o_modifica_emill.sql
+++ prodedimientos/inserta_o_modifica_emill.sql
@@ -1,85 +1,83 @@
-create or replace procedure prc_inserta_t_lineas_intra (
-    v_cd_formulario char,
-    v_cd_atendimiento number,
-    v_fecha_registro date,
-    v_usuario char,
-    v_cd_documento_clinico number,
-    v_fecha_inicio date,
-    v_tipo varchar2,
-    v_localizacion varchar2,
-    v_cuidados clob,
-    v_fecha_curacion date,
-    v_cd_registro_secuencial number
-) is 
-v_sec number;
-begin
--- verifica si es insercion o actualizacion
-if v_cd_registro_secuencial is null then
-  -- calcula el siguiente secuencial
-begin
-  select nvl(max(cd_registro_secuencial),0) +1
-  into v_sec
-  from t_lineas_intravenosas
-  where cd_atendimento = v_cd_atendimiento;
-  
-exception when others then 
-  v_sec := 1;
-  end;
-begin
-insert into
-    t_lineas_intravenosas (
-        cd_formulario,
-        cd_atendimento,
-        fecha_registro,
-        usuario,
-        cd_documento_clinico,
-        fecha_inicio,
-        tipo,
-        localizacion,
-        cuidados,
-        fecha_curacion,
-        cd_registro_secuencial
-    )
-values
-    (
-        v_cd_formulario,
-        v_cd_atendimiento,
-        v_fecha_registro,
-        v_usuario,
-        v_cd_documento_clinico,
-        v_fecha_inicio,
-        v_tipo,
-        v_localizacion,
-        v_cuidados,
-        v_fecha_curacion,
-        v_sec
-    ); 
-exception when others then
-  null;
-end;
-else
-  BEGIN
-    UPDATE t_lineas_intravenosas
-    SET
-        cd_formulario = v_cd_formulario,
-        cd_atendimento = v_cd_atendimiento,
-        fecha_registro = v_fecha_registro,
-        usuario = v_usuario,
-        cd_documento_clinico = v_cd_documento_clinico,
-        fecha_inicio = v_fecha_inicio,
-        tipo = v_tipo,
-        localizacion = v_localizacion,
-        cuidados = v_cuidados,
-        fecha_curacion = v_fecha_curacion
-    WHERE
-        cd_registro_secuencial = v_cd_registro_secuencial
-        and cd_atendimento = v_cd_atendimiento;
-  EXCEPTION
-    WHEN OTHERS THEN
-      NULL;
-  END;
-end if;
-
-commit;
-
-end prc_inserta_t_lineas_intra;
+CREATE OR REPLACE PROCEDURE PRC_INSERTA_T_LINEAS_INTRA (
+  V_CD_FORMULARIO CHAR,
+  V_CD_ATENDIMIENTO NUMBER,
+  V_FECHA_REGISTRO DATE,
+  V_USUARIO CHAR,
+  V_CD_DOCUMENTO_CLINICO NUMBER,
+  V_FECHA_INICIO DATE,
+  V_TIPO VARCHAR2,
+  V_LOCALIZACION VARCHAR2,
+  V_CUIDADOS CLOB,
+  V_FECHA_CURACION DATE,
+  V_CD_REGISTRO_SECUENCIAL NUMBER
+) IS
+  V_SEC NUMBER;
+BEGIN
+ -- verifica si es insercion o actualizacion
+  IF V_CD_REGISTRO_SECUENCIAL IS NULL THEN
+ -- calcula el siguiente secuencial
+    BEGIN
+      SELECT
+        NVL(MAX(CD_REGISTRO_SECUENCIAL), 0) +1 INTO V_SEC
+      FROM
+        T_LINEAS_INTRAVENOSAS
+      WHERE
+        CD_ATENDIMENTO = V_CD_ATENDIMIENTO;
+    EXCEPTION
+      WHEN OTHERS THEN
+        V_SEC := 1;
+    END;
+    BEGIN
+      INSERT INTO T_LINEAS_INTRAVENOSAS (
+        CD_FORMULARIO,
+        CD_ATENDIMENTO,
+        FECHA_REGISTRO,
+        USUARIO,
+        CD_DOCUMENTO_CLINICO,
+        FECHA_INICIO,
+        TIPO,
+        LOCALIZACION,
+        CUIDADOS,
+        FECHA_CURACION,
+        CD_REGISTRO_SECUENCIAL
+      ) VALUES (
+        V_CD_FORMULARIO,
+        V_CD_ATENDIMIENTO,
+        V_FECHA_REGISTRO,
+        V_USUARIO,
+        V_CD_DOCUMENTO_CLINICO,
+        V_FECHA_INICIO,
+        V_TIPO,
+        V_LOCALIZACION,
+        V_CUIDADOS,
+        V_FECHA_CURACION,
+        V_SEC
+      );
+    EXCEPTION
+      WHEN OTHERS THEN
+        NULL;
+    END;
+  ELSE
+    BEGIN
+      UPDATE T_LINEAS_INTRAVENOSAS
+      SET
+        CD_FORMULARIO = V_CD_FORMULARIO,
+        CD_ATENDIMENTO = V_CD_ATENDIMIENTO,
+        FECHA_REGISTRO = V_FECHA_REGISTRO,
+        USUARIO = V_USUARIO,
+        CD_DOCUMENTO_CLINICO = V_CD_DOCUMENTO_CLINICO,
+        FECHA_INICIO = V_FECHA_INICIO,
+        TIPO = V_TIPO,
+        LOCALIZACION = V_LOCALIZACION,
+        CUIDADOS = V_CUIDADOS,
+        FECHA_CURACION = V_FECHA_CURACION
+      WHERE
+        CD_REGISTRO_SECUENCIAL = V_CD_REGISTRO_SECUENCIAL
+        AND CD_ATENDIMENTO = V_CD_ATENDIMIENTO;
+    EXCEPTION
+      WHEN OTHERS THEN
+        NULL;
+    END;
+  END IF;
+  COMMIT;
+END PRC_INSERTA_T_LINEAS_INTRA;

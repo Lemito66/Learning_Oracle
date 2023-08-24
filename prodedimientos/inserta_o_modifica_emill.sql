82a7adb6f92b120a4386dcfc08fac6df563fb511
Emill <lemito66@gmail.com>
2023-08-24 09:24:29 -0500

    actualización o inserción

--- prodedimientos/inserta_o_modifica_emill.sql
+++ prodedimientos/inserta_o_modifica_emill.sql
@@ -13,6 +13,9 @@ create or replace procedure prc_inserta_t_lineas_intra (
 ) is 
 v_sec number;
 begin
+-- verifica si es insercion o actualizacion
+if v_cd_registro_secuencial is null then
+  -- calcula el siguiente secuencial
 begin
   select nvl(max(cd_registro_secuencial),0) +1
   into v_sec
@@ -22,6 +25,7 @@ begin
 exception when others then 
   v_sec := 1;
   end;
+begin
 insert into
     t_lineas_intravenosas (
         cd_formulario,
@@ -49,6 +53,33 @@ values
         v_cuidados,
         v_fecha_curacion,
         v_sec
-    ); commit;
+    ); 
+exception when others then
+  null;
+end;
+else
+  BEGIN
+    UPDATE t_lineas_intravenosas
+    SET
+        cd_formulario = v_cd_formulario,
+        cd_atendimento = v_cd_atendimiento,
+        fecha_registro = v_fecha_registro,
+        usuario = v_usuario,
+        cd_documento_clinico = v_cd_documento_clinico,
+        fecha_inicio = v_fecha_inicio,
+        tipo = v_tipo,
+        localizacion = v_localizacion,
+        cuidados = v_cuidados,
+        fecha_curacion = v_fecha_curacion
+    WHERE
+        cd_registro_secuencial = v_cd_registro_secuencial
+        and cd_atendimento = v_cd_atendimiento;
+  EXCEPTION
+    WHEN OTHERS THEN
+      NULL;
+  END;
+end if;
+
+commit;
 
 end prc_inserta_t_lineas_intra;

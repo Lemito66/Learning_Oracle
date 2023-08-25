8fdc79985076ebc57ce2a0234a02312d3dda1ce3
Emill <lemito66@gmail.com>
2023-08-25 09:57:27 -0500

    recuperación de campos

---
+++ prodedimientos/recuperacion_campos_formulario.sql
@@ -0,0 +1,6 @@
+select * from t_lineas_intravenosas where cd_atendimento = 654
+
+
+select fecha_inicio from editor_custom.t_lineas_intravenosas
+where cd_atendimento = '&<PAR_CD_ATENDIMENTO>' 
+and cd_registro_secuencial = '&<474027:TXT_CODIGO_LINEASINTRAVENOSAS187>'

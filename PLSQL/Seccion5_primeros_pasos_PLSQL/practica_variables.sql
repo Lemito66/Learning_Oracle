4aa93c2bf564175f86a5b7afe33dc5a862728490
Emill <lemito66@gmail.com>
2023-09-01 16:26:22 -0500

    practica variables

---
+++ PLSQL/Seccion5_primeros_pasos_PLSQL/practica_variables.sql
@@ -0,0 +1,10 @@
+-- Crear dos variables de tipo numérico y visualizar su suma
+
+DECLARE
+    v_num1 NUMBER;
+    v_num2 NUMBER;
+BEGIN
+    v_num1 := 10;
+    v_num2 := 20;
+    DBMS_OUTPUT.PUT_LINE('La suma de ' || v_num1 || ' + ' || v_num2 || ' es: ' || (v_num1 + v_num2));
+END;

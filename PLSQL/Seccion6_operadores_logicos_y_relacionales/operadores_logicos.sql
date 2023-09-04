255963e730882148b53884470899df81cb03b34c
Emill <lemito66@gmail.com>
2023-09-04 16:32:58 -0500

    operador logico

---
+++ PLSQL/Seccion6_operadores_logicos_y_relacionales/operadores_logicos.sql
@@ -0,0 +1,15 @@
+/*
+Operadores relacionales o de comparacion
+
+= igual
+<> distinto
+> mayor que
+< menor que
+>= mayor o igual que
+<= menor o igual que
+
+Operadores lógicos
+and 
+not 
+or
+ */

55c94f2a0ba03b02faf1eec5bc09a9d2aeefc28c
Emill <lemito66@gmail.com>
2023-09-11 09:52:53 -0500

    bucle while

--- PLSQL/Seccion6_operadores_logicos_y_relacionales/while.sql
+++ PLSQL/Seccion6_operadores_logicos_y_relacionales/while.sql
@@ -2,8 +2,9 @@
 DECLARE
     CONDITION BOOLEAN := TRUE;
 BEGIN
-    WHILE CONDITION LOOP
+    WHILE CONDITION LOOP -- Podemos usar negacion NOT
         DBMS_OUTPUT.PUT_LINE('Hola');
         CONDITION := FALSE;
+        -- Tambien podemos usar exit when 
     END LOOP;
 END;

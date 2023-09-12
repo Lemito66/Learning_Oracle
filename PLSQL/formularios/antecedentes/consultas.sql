384219a2997d77ce15510cd0b9c88dd1d73af3b0
Emill <lemito66@gmail.com>
2023-09-12 12:18:57 -0500

    más consultas

--- PLSQL/formularios/antecedentes/consultas.sql
+++ PLSQL/formularios/antecedentes/consultas.sql
@@ -2,3 +2,6 @@ select * from t_tipos_antecedentes
 
 
 select * from T_ANTECEDENTE_PERS_FAM
+
+
+select * from T_EMERGENCIA
---
+++ PLSQL/formularios/antecedentes/consultas.~sql
@@ -0,0 +1,4 @@
+select * from t_tipos_antecedentes 
+
+
+select * from T_ANTECEDENTE_PERS_FAM

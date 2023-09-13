9084b050e93e7862c6290e28aaafc9acfa6c0376
Emill <lemito66@gmail.com>
2023-09-13 12:00:15 -0500

    consultas a la tabla t_emergencia

--- PLSQL/formularios/antecedentes/consultas.sql
+++ PLSQL/formularios/antecedentes/consultas.sql
@@ -4,4 +4,7 @@ select * from t_tipos_antecedentes
 select * from T_ANTECEDENTE_PERS_FAM
 
 
-select * from T_EMERGENCIA
+
+select * from T_EXAMEN_FISICO where cd_atendimento = 301  -- Formulario de examen fisico
+
+select * from t_emergencia  --formulario antecedentes

6b0e6b957433dd6d3a1768cdf598712beaa52b9c
Emill <lemito66@gmail.com>
2023-10-03 15:59:38 -0500

    consulta valoración inicial

---
+++ PLSQL/formularios/valoracion_inicial/consultas_valoración_inicial.sql
@@ -0,0 +1,13 @@
+select * from t_examen_fisico where cd_atendimento = 668
+
+select * from t_examen_fisico where cd_atendimento = 654
+
+t_emergencia
+
+delete from t_examen_fisico where t_examen_fisico.observaciones_examen_fisico = 'ggg'
+
+
+select * from t_examen_fisico where t_examen_fisico.observaciones_examen_fisico = 'ggg'
+
+
+select * from t_emergencia

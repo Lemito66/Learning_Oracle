ae76d83ee800d5160cc659ddf1d6fb3038a69075
Emill <lemito66@gmail.com>
2023-10-04 12:42:42 -0500

    notas para hacer en el futuro

--- PLSQL/formularios/valoracion_inicial/tabla_t_emergencia.sql
+++ PLSQL/formularios/valoracion_inicial/tabla_t_emergencia.sql
@@ -5,3 +5,8 @@ alter table T_EMERGENCIA add inestable VARCHAR2(10);
 alter table T_EMERGENCIA add fallecido VARCHAR2(10);
 alter table T_EMERGENCIA add pelvis_viable VARCHAR2(20);
 alter table T_EMERGENCIA add score_mama VARCHAR2(20);
+
+
+
+-- Se hizo cambios en el procedimiento que está en el archivo procedimiento_inserta_t_emergencia.sql
+-- Se hizo cambios en la regla que está en el arhivo regla.sql

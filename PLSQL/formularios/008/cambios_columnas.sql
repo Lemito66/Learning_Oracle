48f625e4139263e8822af13760aeb71ba081f795
Emill <lemito66@gmail.com>
2023-10-06 16:58:52 -0500

    cambios momentaneos por columnas

---
+++ PLSQL/formularios/008/cambios_columnas.sql
@@ -0,0 +1,13 @@
+-- Add/modify columns 
+alter table T_TMP_008 modify campo14 VARCHAR2(10);
+alter table T_TMP_008 modify campo22 VARCHAR2(20);
+alter table T_TMP_008 modify campo23 clob;
+alter table T_TMP_008 modify campo36 VARCHAR2(200);
+alter table T_TMP_008 modify campo37 VARCHAR2(200);
+alter table T_TMP_008 modify campo38 VARCHAR2(50);
+alter table T_TMP_008 modify campo39 VARCHAR2(50);
+alter table T_TMP_008 modify campo40 VARCHAR2(200);
+alter table T_TMP_008 modify campo45 VARCHAR2(50);
+alter table T_TMP_008 modify campo46 VARCHAR2(100);
+alter table T_TMP_008 modify campo47 VARCHAR2(50);
+alter table T_TMP_008 modify campo48 VARCHAR2(50);

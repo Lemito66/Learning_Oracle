bdf755a2bbb2ba8cab0b67144b03f793cc1f3702
Emill <lemito66@gmail.com>
2023-10-11 16:34:39 -0500

    ALIAS

--- PLSQL/formularios/008/grupo_prioritario.sql
+++ PLSQL/formularios/008/grupo_prioritario.sql
@@ -2,5 +2,5 @@ select FN_IDADE(DT_NASCIMENTO),
 -- Calcular la edad de un paciente y si es de 0 a 18 o mayor a 60 salga grupo prioritario
 case when FN_IDADE(DT_NASCIMENTO) between 0 and 18 then 'Grupo Prioritario'
 when FN_IDADE(DT_NASCIMENTO) > 60 then 'Grupo Prioritario'
-else 'No es Grupo Prioritario' end as 'Grupo Prioritario' 
+else 'No es Grupo Prioritario' end GRUPO_PRIORITARIO 
 from paciente

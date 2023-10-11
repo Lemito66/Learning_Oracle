e77c9b68e246008657351e567c2ebfaef0638dc1
Emill <lemito66@gmail.com>
2023-10-11 15:03:10 -0500

    solo select actualizado

--- PLSQL/formularios/008/solo_select_campos_paciente_atencion.sql
+++ PLSQL/formularios/008/solo_select_campos_paciente_atencion.sql
@@ -1,23 +1,23 @@
 SELECT
     B.DS_MULTI_EMPRESA INSTITUCION_DEL_SISTEMA, --1
     '065472' UNICODIGO, --2
-    -- Estamos a la espera de si el establecimiento de salud es igual a institución del sistema
+    -- Estamos a la espera de si el establecimiento de salud es igual a instituciÃ³n del sistema
     DECODE(A.TP_ATENDIMENTO, 'U', 'EMERGENCIA', '') UNIDAD_OPERATIVA,
     NVL(
         E.CD_IDENTIFICADOR_PESSOA,
         E.NR_DOCUMENTO_ESTRANGEIRO
     ) CEDULA_CIUDADANIA_PACIENTE, -- 4
     A.CD_ATENDIMENTO NUMERO_DE_ARCHIVO, -- 5
-    TO_CHAR(A.HR_ATENDIMENTO, 'dd/mm/yyyy') FECHA_ADMISIÓN, -- 6
+    TO_CHAR(A.HR_ATENDIMENTO, 'dd/mm/yyyy') FECHA_ADMISIÃ“N, -- 6
     -- 7 por confirmar
     E.DS_PRIMEIRO_SOBRENOME APELLIDO_PATERNO_PACIENTE, --8
     E.DS_SEGUNDO_SOBRENOME APELLIDO_MATERNO_PACIENTE, -- 9
     E.DS_PRIMEIRO_NOME PRIMER_NOMBRE_PACIENTE, -- 10
     E.DS_SEGUNDO_NOME SEGUNDO_NOMBRE_PACIENTE, -- 11
     CASE
-        WHEN E.CD_IDENTIFICADOR_PESSOA IS NOT NULL THEN 'CÉDULA'
+        WHEN E.CD_IDENTIFICADOR_PESSOA IS NOT NULL THEN 'CÃ‰DULA'
         WHEN E.NR_DOCUMENTO_ESTRANGEIRO IS NOT NULL THEN 'PASAPORTE'
-        ELSE NULL -- Otra opción si ninguno de los campos tiene valor
+        ELSE NULL -- Otra opciÃ³n si ninguno de los campos tiene valor
     END AS CEDULA_CIUDADANIA_PACIENTE, -- 12
     DECODE(
         E.TP_ESTADO_CIVIL,
@@ -38,15 +38,61 @@ SELECT
     TO_CHAR(E.DT_NASCIMENTO, 'dd/mm/yyyy') FECHA_NACIMIENTO_PACIENTE, -- 17
     F.NM_CIDADE LUGAR_NACIMIENTO_PACIENTE, -- 18
     H.DS_CIDADANIA NACIONALIDAD_PACIENTE, -- 19
-    FN_IDADE(E.DT_NASCIMENTO,'a "años", m "meses", d "días"')EDAD_PACIENTE -- 20
+    FN_IDADE(E.DT_NASCIMENTO,'a "aÃ±os", m "meses", d "dÃ­as"')EDAD_PACIENTE, -- 20
     -- 21 por preguntar
     -- 22 por preguntar
     -- 23 por preguntar
+    DECODE(
+        E.TP_COR,
+        'S',
+        'SIN INFORMACIÃ“N',
+        'R',
+        'MONTUBIO',
+        'P',
+        'MESTIZO',
+        'I',
+        'INDIGENA',
+        'B',
+        'BLANCO',
+        'A',
+        'AFROECUATORIANO'
+    ) GRUPO_CULTURAL_PACIENTE, -- 24
+    DECODE(
+        E.TP_COR,
+        'S',
+        'SIN INFORMACIÃ“N',
+        'R',
+        'MONTUBIO',
+        'P',
+        'MESTIZO',
+        'I',
+        'INDIGENA',
+        'B',
+        'BLANCO',
+        'A',
+        'AFROECUATORIANO'
+    ) GRUPO_CULTURAL_PACIENTE, -- 25
+    null PUEBLOS, -- 26
+    I.DS_GRAU_INS INSTRUCCION_PACIENTE, -- 27
+    null ESTADO_NIVEL_DE_EDUCACION, -- 28
+    E.DS_TRABALHO EMPRESA_TRABAJO_PACIENTE, -- 29
+    J.NM_PROFISSAO OCUPACION_PACIENTE, -- 30
+    K.NM_CONVENIO CONVENIO_PACIENTE, -- 31
+    G.NM_ESTADO PROVINCIA_PACIENTE, -- 32
+    F.NM_CIDADE CANTON_PACIENTE, -- 33
+    E.NM_BAIRRO PARROQUIA_PACIENTE, -- 34
+    NULL BARRIO_PACIENTE, -- 35
+    E.DS_ENDERECO DIRECCION_RESIDENCIAL_PACIENTE, -- 36
+    null REFERENCIA, -- 37
+    M.NM_RESPONSAVEL AVISAR_A_PACIENTE, -- 38
+    N.DS_TIP_PAREN PARIENTE_PACIENTE, -- 39
+    M.DS_ENDERECO DIRECCION_PARIENTE_PACIENTE, -- 40
+    M.NR_FONE TELEFONO_PACIENTE_PACIENTE -- 41
     /* 'BELISARIO QUEVEDO' PARROQUIA_EMPRESA,
     C.NM_CIDADE CANTON_EMPRESA,
     D.NM_ESTADO PROVINCIA_EMPRESA,
     E.CD_PACIENTE NHCL,
-    A.CD_ATENDIMENTO ADMISIÓN,
+    A.CD_ATENDIMENTO ADMISIÃ“N,
     DECODE(A.TP_MEDICO_ELEITO, 'H', 'HOSPITAL', 'PARTICULAR') TIPO_PACIENTE,
     E.DS_PRIMEIRO_SOBRENOME APELLIDO_PATERNO_PACIENTE,
     E.DS_SEGUNDO_SOBRENOME APELLIDO_MATERNO_PACIENTE,
@@ -66,7 +112,7 @@ SELECT
     DECODE(
         E.TP_COR,
         'S',
-        'SIN INFORMACIÓN',
+        'SIN INFORMACIÃ“N',
         'R',
         'MONTUBIO',
         'P',
@@ -94,7 +140,7 @@ SELECT
         'UNION LIBRE'
     ) ESTADO_CIVIL_PACIENTE,
     I.DS_GRAU_INS INSTRUCCION_PACIENTE,
-    TO_CHAR(A.HR_ATENDIMENTO, 'dd/mm/yyyy') FECHA_ADMISIÓN,
+    TO_CHAR(A.HR_ATENDIMENTO, 'dd/mm/yyyy') FECHA_ADMISIÃ“N,
     J.NM_PROFISSAO OCUPACION_PACIENTE,
     E.DS_TRABALHO EMPRESA_TRABAJO_PACIENTE,
     K.NM_CONVENIO CONVENIO_PACIENTE,
@@ -151,25 +197,26 @@ SELECT
     V_CAMPO48,
     V_CAMPO53 */
 FROM
-    ATENDIME A, -- tabla de la atención
-    MULTI_EMPRESAS B, -- tabla de la empresa, aquí está hospital metroplitano
+    ATENDIME A, -- tabla de la atenciÃ³n
+    MULTI_EMPRESAS B, -- tabla de la empresa, aquÃ­ estÃ¡ hospital metroplitano
     CIDADE C, -- Ciudades
     ESTADO D, -- Provincias
     PACIENTE E, -- Datos del paciente
     CIDADE F, -- Ciudades
     ESTADO G, -- Provincia
     CIDADANIAS H, -- Nacionalidad
-    GRAU_INS I, -- Grado de instrucción, ingeniero, bachiller, etc.
+    GRAU_INS I, -- Grado de instrucciÃ³n, ingeniero, bachiller, etc.
     PROFISSAO J, -- profesiones
-    CONVENIO K, -- convenios por seguro médico
+    CONVENIO K, -- convenios por seguro mÃ©dico
     LOC_PROCED L, -- por confirmar
     RESPONSA M, -- Responsable
     TIP_PAREN N, -- tipo de parentesco, madre,padre,chofe,amigo, etc.
     MEIO_TRANSPORTE O -- En que medio de transporte llego el paciente.
+    --T_EMERGENCIA P -- tabla de emergencia
 WHERE
     A.CD_MULTI_EMPRESA = B.CD_MULTI_EMPRESA (+)
     AND B.CD_CIDADE = C.CD_CIDADE (+)
-    AND A.CD_ATENDIMENTO = 98699
+    AND A.CD_ATENDIMENTO = VATENDIMENTO
     AND C.CD_ESTADO = D.CD_ESTADO (+)
     AND A.CD_PACIENTE = E.CD_PACIENTE (+)
     AND E.CD_CIDADE = F.CD_CIDADE (+)

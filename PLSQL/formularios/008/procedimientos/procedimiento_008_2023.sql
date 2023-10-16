ad0bfadcbbc8af214b297e0692af459835b1b3b1
Emill <lemito66@gmail.com>
2023-10-16 15:13:44 -0500

    insertando exámen físico

--- PLSQL/formularios/008/procedimientos/procedimiento_008_2023.sql
+++ PLSQL/formularios/008/procedimientos/procedimiento_008_2023.sql
@@ -715,213 +715,181 @@ END;
 
 -- Se elimino campos formularios
 
--- ANTECEDENTE, EXAMEN FISICO
+-- ANTECEDENTE,  Solo EXAMEN FISICO
 BEGIN
 SELECT
-    'true' ALERGICO,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_ANTECEDENTE_PERS_FAM
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND TIPO_ANTECEDENTE = 'CLÍNICO'
-        ),
-        'false'
-    ) CLINICO,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_ANTECEDENTE_PERS_FAM
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND TIPO_ANTECEDENTE = 'GINECOLÓGICO'
-        ),
-        'false'
-    ) GINECOLOGICO,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_ANTECEDENTE_PERS_FAM
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND TIPO_ANTECEDENTE = 'TRAUMATOLÓGICO'
-        ),
-        'false'
-    ) TRAUMATOLOGICO,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_ANTECEDENTE_PERS_FAM
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND TIPO_ANTECEDENTE = 'QUIRÚRGICO'
-        ),
-        'false'
-    ) QUIRURGICO,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_ANTECEDENTE_PERS_FAM
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND TIPO_ANTECEDENTE = 'FARMACOLÓGICO'
-        ),
-        'false'
-    ) FARMACOLOGICO,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_ANTECEDENTE_PERS_FAM
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND TIPO_ANTECEDENTE = 'PEDIÁTRICO'
-        ),
-        'false'
-    ) PEDIATRICO,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_ANTECEDENTE_PERS_FAM
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND TIPO_ANTECEDENTE IN ('OTRO', 'NATURÓPATA')
-        ),
-        'false'
-    ) OTRO,
-    FUN_ANTECEDENTES_MEDICOS (A.CD_ATENDIMENTO) ANTECEDENTES_4,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_EXAMEN_FISICO A
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND A.TIPO_EXAMEN_FISICO = 'VÍA AÉREA OBSTRUÍDA'
-        ),
-        'false'
-    ) VIA_AREA_OBSTRUIDA,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_EXAMEN_FISICO A
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND A.TIPO_EXAMEN_FISICO = 'CABEZA'
-        ),
-        'false'
-    ) CABEZA,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_EXAMEN_FISICO A
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND A.TIPO_EXAMEN_FISICO = 'CUELLO'
-        ),
-        'false'
-    ) CUELLO,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_EXAMEN_FISICO A
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND A.TIPO_EXAMEN_FISICO = 'TÓRAX'
-        ),
-        'false'
-    ) TORAX,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_EXAMEN_FISICO A
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND A.TIPO_EXAMEN_FISICO = 'ABDOMEN'
-        ),
-        'false'
-    ) ABDOMEN,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_EXAMEN_FISICO A
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND A.TIPO_EXAMEN_FISICO = 'COLUMNA'
-        ),
-        'false'
-    ) COLUMNA,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_EXAMEN_FISICO A
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND A.TIPO_EXAMEN_FISICO = 'PELVIS'
-        ),
-        'false'
-    ) PELVIS,
-    NVL(
-        (
-            SELECT
-                DECODE(COUNT(*), 0, 'false', 'true')
-            FROM
-                T_EXAMEN_FISICO A
-            WHERE
-                CD_ATENDIMENTO = A.CD_ATENDIMENTO
-                AND A.TIPO_EXAMEN_FISICO = 'EXTREMIDADES'
-        ),
-        'false'
-    ) EXTREMIDADES,
-    FUN_EXAMEN_FISICO_008 (A.CD_ATENDIMENTO) OBSERVACIONES_7 INTO V_CAMPO81,
-    V_CAMPO82,
-    V_CAMPO83,
-    V_CAMPO84,
-    V_CAMPO85,
-    V_CAMPO86,
-    V_CAMPO87,
-    V_CAMPO88,
-    V_CAMPO89,
-    V_CAMPO112,
-    V_CAMPO113,
-    V_CAMPO114,
-    V_CAMPO115,
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'PIEL - FANERAS'
+    ) PIEL_FANERAS, -- 116
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'CABEZA'
+    ) CABEZA, -- 117
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'OJOS'
+    ) OJOS, -- 118
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'OÍDOS'
+    ) OIDOS, -- 119
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'NARIZ'
+    ) NARIZ, -- 120
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'BOCA'
+    ) BOCA, -- 121
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'ORO FARINGE'
+    ) ORO_FARINGE, -- 122
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'CUELLO'
+    ) CUELLO, -- 123
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'AXILAS - MAMAS'
+    ) AXILAS_MAMAS, -- 124
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'TÓRAX'
+    )TORAX, -- 125
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'ABDOMEN'
+    ) ABDOMEN, -- 126
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'COLUMNA VERTEBRAL'
+    ) COLUMNA_VERTEBRAL, -- 127
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'INGLE - PERINÉ'
+    ) INGRE_PERINE, -- 128
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'MIEMBROS SUPERIORES'
+    ) MIEMBROS_SUPERIORES, -- 129
+    (
+        SELECT
+            DECODE(A.observaciones_examen_fisico, NULL, NULL, 'X')
+            
+        FROM
+            T_EXAMEN_FISICO A
+        WHERE
+            CD_ATENDIMENTO = VATENDIMENTO --VATENDIMENTO
+            AND A.TIPO_EXAMEN_FISICO = 'MIEMBROS INFERIORES'
+    ) MIEMBROS_INFERIORES, -- 130
+    FUN_EXAMEN_FISICO_008 (VATENDIMENTO)  OBSERVACIONES_EXAMEN_FISICO -- 131 --VATENDIMENTO
+
+    INTO 
     V_CAMPO116,
     V_CAMPO117,
     V_CAMPO118,
     V_CAMPO119,
-    V_CAMPO120
+    V_CAMPO120,
+    V_CAMPO121,
+    V_CAMPO122,
+    V_CAMPO123,
+    V_CAMPO124,
+    V_CAMPO125,
+    V_CAMPO126,
+    V_CAMPO127,
+    V_CAMPO128,
+    V_CAMPO129,
+    V_CAMPO130,
+    V_CAMPO131
+    
 FROM
-    ATENDIME A
-WHERE
-    A.CD_ATENDIMENTO = VATENDIMENTO;
+    DUAL;
 
 EXCEPTION WHEN OTHERS THEN NULL;
 

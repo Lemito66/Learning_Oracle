473b973b8061e63e71d60f3e3de35ccc4db019f6
Emill <lemito66@gmail.com>
2023-10-18 16:14:00 -0500

    custodia policial

--- PLSQL/formularios/008/procedimientos/procedimiento_008_2023.sql
+++ PLSQL/formularios/008/procedimientos/procedimiento_008_2023.sql
@@ -471,8 +471,9 @@ SELECT
     -- 45 No hay que traer este campo
     null INSTITUCIONENTREGA_AL_PACIENTE, -- 46
     M.NR_FONE TELEFONO_PACIENTE_PACIENTE, -- 47
-    E.TP_SANGUINEO GRUPO_SANGUINEO_PACIENTE -- 57
-    /* INTO 
+    E.TP_SANGUINEO GRUPO_SANGUINEO_PACIENTE, -- 57
+    DECODE(A.SN_CUSTODIA_POLICIAL, 'N', null, 'X') CUSTODIA_POLICIAL -- 61         
+    INTO 
     V_CAMPO1,
     V_CAMPO2,
     V_CAMPO3,
@@ -517,7 +518,8 @@ SELECT
     V_CAMPO42,
     V_CAMPO46,
     V_CAMPO47,
-    V_CAMPO57 */
+    V_CAMPO57,
+    V_CAMPO61
 FROM
     ATENDIME A, -- tabla de la atención
     MULTI_EMPRESAS B, -- tabla de la empresa, aquí está hospital metroplitano

d200751626cf15c70f026a4fc18324583de98e09
Emill <lemito66@gmail.com>
2023-11-05 21:25:21 -0500

    select signos vitales

---
+++ PLSQL/formularios/008/select/select_signo_vital.sql
@@ -0,0 +1,7 @@
+select * from sinal_vital where cd_sinal_vital = 4 or cd_sinal_vital = 5
+
+select * from sinal_vital where cd_sinal_vital = 43
+
+select * from sinal_vital where ds_sinal_vital like '%IZQUIERDO%'
+
+

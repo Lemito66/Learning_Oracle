d623f9d79c94d20f5663504dd1b1a17d57b9c3bd
Lemito66 <lemito66@gmail.com>
2023-11-16 15:05:50 -0500

    formateo

--- PLSQL/formularios/008/procedimientos/PRC_INSERTA_DESTINO_008.sql
+++ PLSQL/formularios/008/procedimientos/PRC_INSERTA_DESTINO_008.sql
@@ -1,51 +1,70 @@
-create or replace procedure PRC_INSERTA_DESTINO_008(vAtendimento NUMBER, vDestino1 CHAR, vDestino2 CHAR,
-                                                    vDestino3 CHAR, vDestino4 CHAR, vDestino5 CHAR, vDestino6 CHAR, 
-                                                    vDestino7 CHAR, vDestino8 CHAR, vDestino9 CHAR, vDestino10 CHAR) is
-  nMaximo NUMBER;
-  vDestino VARCHAR2(100);
+create
+or replace procedure PRC_INSERTA_DESTINO_008 (
+    vAtendimento NUMBER,
+    vDestino1 CHAR,
+    vDestino2 CHAR,
+    vDestino3 CHAR,
+    vDestino4 CHAR,
+    vDestino5 CHAR,
+    vDestino6 CHAR,
+    vDestino7 CHAR,
+    vDestino8 CHAR,
+    vDestino9 CHAR,
+    vDestino10 CHAR
+) is nMaximo NUMBER;
+
+vDestino VARCHAR2(100);
+
 begin
-  -- ENCUENTRA EL ULTIMO REGISTRO
-  begin
-    select max(a.cd_ultimo_registro)
-    into nMaximo
-    from t_emergencia a
-    where cd_atendimento=vAtendimento;
-  exception when others then
-    nMaximo:=null;
-  end;
-  
-  -- ESCOGE EL DESTINO
-  if trim(vDestino1)='true' then
-    vDestino:='Domicilio';
-  elsif trim(vDestino2)='true' then
-    vDestino:='Consulta externa';
-  elsif trim(vDestino3)='true' then
-    vDestino:='Observación';
-  elsif trim(vDestino4)='true' then
-    vDestino:='Referencia';
-  elsif trim(vDestino5)='true' then
-    vDestino:='Abandono Voluntario';
-  elsif trim(vDestino6)='true' then
-    vDestino:='Abandono sin autorización';
-  elsif trim(vDestino7)='true' then
-    vDestino:='Morgue';
-  elsif trim(vDestino8)='true' then
-    vDestino:='Funeraria';
-  elsif trim(vDestino9)='true' then
-    vDestino:='Medicina Legal';
-  elsif trim(vDestino10)='true' then
-    vDestino:='Internación';
-  end if;
-  
-  -- GRABA EL DESTINO EN EL ULTIMO REGISTRO
-  begin
-    update t_emergencia
-    set destino=vDestino
-    where cd_ultimo_registro=nMaximo and
-          cd_atendimento=vAtendimento;
-  exception when others then
-    null;
-  end;
-  
-  commit;
+-- ENCUENTRA EL ULTIMO REGISTRO
+begin
+select
+    max(a.cd_ultimo_registro) into nMaximo
+from
+    t_emergencia a
+where
+    cd_atendimento = vAtendimento;
+
+exception when others then nMaximo := null;
+
+end;
+
+-- ESCOGE EL DESTINO
+if trim(vDestino1) = 'true' then vDestino := 'Domicilio';
+
+elsif trim(vDestino2) = 'true' then vDestino := 'Consulta externa';
+
+elsif trim(vDestino3) = 'true' then vDestino := 'Observación';
+
+elsif trim(vDestino4) = 'true' then vDestino := 'Referencia';
+
+elsif trim(vDestino5) = 'true' then vDestino := 'Abandono Voluntario';
+
+elsif trim(vDestino6) = 'true' then vDestino := 'Abandono sin autorización';
+
+elsif trim(vDestino7) = 'true' then vDestino := 'Morgue';
+
+elsif trim(vDestino8) = 'true' then vDestino := 'Funeraria';
+
+elsif trim(vDestino9) = 'true' then vDestino := 'Medicina Legal';
+
+elsif trim(vDestino10) = 'true' then vDestino := 'Internación';
+
+end if;
+
+-- GRABA EL DESTINO EN EL ULTIMO REGISTRO
+begin
+update t_emergencia
+set
+    destino = vDestino
+where
+    cd_ultimo_registro = nMaximo
+    and cd_atendimento = vAtendimento;
+
+exception when others then null;
+
+end;
+
+commit;
+
 end PRC_INSERTA_DESTINO_008;

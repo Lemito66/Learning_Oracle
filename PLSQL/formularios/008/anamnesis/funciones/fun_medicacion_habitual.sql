29f713bf31418b39b36cf5cfb22da54197730c06
Lemito66 <lemito66@gmail.com>
2023-11-23 09:16:13 -0500

    funcion para medicacion habitual

---
+++ PLSQL/formularios/008/anamnesis/funciones/fun_medicacion_habitual.sql
@@ -0,0 +1,20 @@
+create
+or replace function fun_medicacion_habitual (vPaciente number) return number is vResultado number;
+
+begin
+begin
+select
+  count(*) into vResultado
+from
+  T_ANTECEDENTE_PERS_FAM t
+where
+  t.cd_atendimento = vPaciente -- numero de atencion
+  and t.medicina is not null;
+
+exception when others then vResultado := 0;
+
+end;
+
+return vResultado;
+
+end fun_medicacion_habitual;

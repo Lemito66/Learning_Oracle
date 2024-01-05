541e4e09741b9c2bcc276e441d051472e929565b
Emill <lemito66@gmail.com>
2024-01-05 09:30:12 -0500

    interconsulta

---
+++ PLSQL/formularios/neonatal/interconsulta.sql
@@ -0,0 +1,20 @@
+select
+    e.ds_especialid servicio_interconsultado,
+    to_char(a.dt_solicitacao, 'dd/mm/yyyy hh24:mi') fecha_solicitud,
+    to_char(a.dt_parecer, 'dd/mm/yyyy hh24:mi') fecha_respuesta
+from
+    par_med a,
+    prestador b,
+    prestador c,
+    pw_documento_clinico pw,
+    especialid e
+where
+    a.cd_atendimento = 104138
+    and a.cd_prestador = b.cd_prestador
+    and a.cd_prestador_requisitado = c.cd_prestador
+    and a.ds_situacao <> 'Cancelado'
+    and a.cd_documento_clinico = pw.cd_documento_clinico
+    and pw.tp_status = 'FECHADO'
+    and a.cd_especialid = e.cd_especialid
+order by
+    a.dt_solicitacao desc;

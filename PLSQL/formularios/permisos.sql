1035e32e6af84b74dd67ac8b10f362b6f6cf8b31
Lemito66 <lemito66@gmail.com>
2024-01-18 11:52:51 -0500

    permisos para jasperept

---
+++ PLSQL/formularios/permisos.sql
@@ -0,0 +1,23 @@
+GRANT EXECUTE ON EDITOR_CUSTOM.FUN_SIGNOS_VITALES_U TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_alergias_linea TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.FUN_VALIDA_FECHA_HORA TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_signos_vitales_imc TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_dignosticos_ingreso TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_diagnostico_definitivo TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_prescripcion_mdn_mdo_mda TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.FUN_PRESCRIPCION_BOMBAS TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.FUN_PRESCRIPCION_HEM TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.FUN_PRESCRIPCION_EXI TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.FUN_PRESCRIPCION_EXL TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_prescripcion_interconsulta TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_prescripcion_nutri_enteral TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_prescripcion_procedi TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_prescripcion_proced_enf TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_prescripcion_educacion TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_prescripcion_bomb_infu TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_prescripcion_apoyo_venti TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.fun_prescripcion_otros TO jasperept;
+GRANT EXECUTE ON EDITOR_CUSTOM.FUN_SIGNOS_VITALES_p TO jasperept;
+
+
+

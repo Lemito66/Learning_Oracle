0359d4e372e74dfc52f78d0790ae24bb7ba39d09
Emill <lemito66@gmail.com>
2024-01-24 12:08:02 -0500

    funcion para visualizar los datos en el reporte de uci

---
+++ PLSQL/formularios/medicacion_subcutanea/funcion_tabla.sql
@@ -0,0 +1,32 @@
+create
+or replace function FUN_PRO_MEDI_SUBCU (vAtendimento NUMBER) return clob is cursor subcutanea is
+select
+    rpad(
+        to_char(FECHA_REGISTRO, 'dd/mm/yyyy hh24:mi'),
+        20,
+        ' '
+    ) Fecha,
+    rpad(USUARIO, 25, ' ') Usuario,
+    rpad(APLICACION, 45, ' ') Aplicacion,
+    rpad(comentario, 90, ' ') Comentario
+from
+    T_PRO_MEDICACION_SUBCU
+where
+    CD_ATENDIMENTO = vAtendimento
+order by
+    3,
+    4;
+
+vResultado clob;
+
+begin vResultado := rpad('FECHA', 25, ' ') || rpad('USUARIO', 25, ' ') || rpad('APLICACIÓN', 85, ' ') || rpad('COMENTARIO', 90, ' ') || chr(10);
+
+for i in subcutanea
+loop vResultado := vResultado || i.Fecha || i.Usuario || i.Aplicacion || i.Comentario || chr(10);
+
+end
+loop;
+
+return (vResultado);
+
+end FUN_PRO_MEDI_SUBCU;

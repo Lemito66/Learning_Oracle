8973b974ffbf63360cf65b13cf92a03e4e289f49
Emill <lemito66@gmail.com>
2024-03-01 13:17:52 -0500

    modificación de query ya que ahora solo se va a tomar el primer valor

--- PLSQL/formularios/atencion_inmediata_recien_nacido/formulario_final/fun_signos_vitales_por_4.sql
+++ PLSQL/formularios/atencion_inmediata_recien_nacido/formulario_final/fun_signos_vitales_por_4.sql
@@ -1,50 +1,47 @@
-create
-or replace function fun_signos_vitales_4 (vAtendimento NUMBER, vSigno NUMBER) return NVARCHAR2 IS cursor data is
-select
-    -- rownum,
-    i.data_coleta,
-    h.valor
-from
-    dbamv.itcoleta_sinal_vital h,
-    dbamv.coleta_sinal_vital i,
-    dbamv.sinal_vital j,
-    pw_documento_clinico pw
-where
-    i.cd_atendimento = 19652
-    and h.cd_coleta_sinal_vital = i.cd_coleta_sinal_vital
-    and h.cd_sinal_vital = j.cd_sinal_vital
-    and h.cd_sinal_vital in 7
-    and --4
-    h.valor is not null
-    and i.cd_documento_clinico = pw.cd_documento_clinico
-    and pw.tp_status = 'FECHADO'
-order by
-    i.data_coleta desc
-FETCH FIRST
-    4 ROWS ONLY;
+CREATE
+OR REPLACE FUNCTION fun_signos_vitales_4 (vAtendimento NUMBER, vSigno NUMBER) RETURN NVARCHAR2 IS v_fecha_hora VARCHAR2(20);
 
-    v_resultado NVARCHAR2(4000);
+v_valor dbamv.itcoleta_sinal_vital.valor % TYPE;
 
-    -- variables para almacenar los valores de los signos vitales
-    v_fecha dbamv.coleta_sinal_vital.data_coleta%TYPE;
-    v_valor dbamv.itcoleta_sinal_vital.valor%TYPE;
+v_nombre_signo dbamv.sinal_vital.ds_sinal_vital % TYPE;
 
-    -- variable para el contador 
-    v_contador NUMBER := 1;
+v_nomenclatura NVARCHAR2(100);
+
+v_resultado NVARCHAR2(4000);
 
 BEGIN
-    v_resultado := '';
-     
-    open data;
+SELECT
+    TO_CHAR(i.data_coleta, 'DD/MM/YYYY HH24:MI:SS'),
+    h.valor,
+    j.ds_sinal_vital,
+    CASE vSigno
+        WHEN 1 THEN 'grados centígrados'
+        WHEN 2 THEN '/ minuto'
+        WHEN 3 THEN '/ minuto'
+        WHEN 4 THEN 'mmHg'
+        WHEN 5 THEN 'mmHg'
+        WHEN 7 THEN '%'
+    END INTO v_fecha_hora,
+    v_valor,
+    v_nombre_signo,
+    v_nomenclatura
+FROM
+    dbamv.itcoleta_sinal_vital h
+    JOIN dbamv.coleta_sinal_vital i ON h.cd_coleta_sinal_vital = i.cd_coleta_sinal_vital
+    JOIN dbamv.sinal_vital j ON h.cd_sinal_vital = j.cd_sinal_vital
+    JOIN pw_documento_clinico pw ON i.cd_documento_clinico = pw.cd_documento_clinico
+WHERE
+    i.cd_atendimento = vAtendimento
+    AND h.cd_sinal_vital = vSigno
+    AND h.valor IS NOT NULL
+    AND pw.tp_status = 'FECHADO'
+ORDER BY
+    i.data_coleta ASC
+FETCH FIRST
+    1 ROWS ONLY;
 
-    loop
-        fetch data into v_fecha, v_valor;
-        exit when data%NOTFOUND;
-        v_resultado := v_resultado || v_contador || '. ' || 'Fecha: ' || v_fecha || ' - Valor: ' || v_valor || chr(10);
-        v_contador := v_contador + 1;
-    end loop;
+v_resultado := 'Nombre signo: ' || v_nombre_signo || CHR(10) || 'Fecha y hora: ' || v_fecha_hora || ' - Valor: ' || v_valor || ' ' || v_nomenclatura || CHR(10) || CHR(10);
 
-    close data;
+RETURN v_resultado;
 
-    return v_resultado;
 END fun_signos_vitales_4;

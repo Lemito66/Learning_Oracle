2ae6095b64983cbc8f7e5567a3d098cfc474fbcd
Lemito66 <lemito66@gmail.com>
2024-03-25 11:31:13 -0500

    consultas principales a la tabla de terapia respiratoria

---
+++ PLSQL/formularios/terapia_respiratoria/consultas_pricipales.sql
@@ -0,0 +1,51 @@
+select
+    count(*)
+from
+    formularioterapiarespiratoria
+
+select
+    TO_date(
+        JSON_VALUE (terapia.Data, '$.FECHAHOY'),
+        'DD/MM/YYYY'
+    )
+from
+    formularioterapiarespiratoria terapia
+
+
+select
+    *
+from
+    formularioterapiarespiratoria
+where
+    id = 9062
+
+
+select
+    terapia.numerodepedido,
+    terapia.estado,
+    terapia.id,
+    JSON_VALUE (terapia.Data, '$.FECHAHOY')
+from
+    formularioterapiarespiratoria terapia
+where
+    TO_date(
+        JSON_VALUE (terapia.Data, '$.FECHAHOY'),
+        'DD/MM/YYYY'
+    ) = Trunc(sysdate)
+
+
+
+SELECT
+    terapia.numerodepedido,
+    terapia.estado,
+    terapia.id,
+    JSON_VALUE (terapia.Data, '$.FECHAHOY') AS fecha_json
+FROM
+    formularioterapiarespiratoria terapia
+WHERE
+    trunc(
+        TO_DATE(
+            JSON_VALUE (terapia.Data, '$.FECHAHOY'),
+            'DD-MM-YYYY HH24:MI'
+        )
+    ) = TRUNC(SYSDATE);

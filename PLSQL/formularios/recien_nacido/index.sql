dbeccbd4aa37189ea60f708713756c0fb6becc2a
Emill <lemito66@gmail.com>
2024-04-04 11:55:58 -0500

    consultas necesarios para el recien nacido

--- PLSQL/formularios/recien_nacido/index.sql
+++ PLSQL/formularios/recien_nacido/index.sql
@@ -1,3 +1,40 @@
+EXECUTE dbamv.pkg_mv2000.atribui_empresa(1);
+
+BEGIN
+  dbamv.pkg_mv2000.atribui_empresa(1);
+END;
+
+
+update paciente 
+set
+    --cd_identificador_pessoa = '0602268500',
+    cd_identificador_pessoa = '0604312801',
+    ds_primeiro_sobrenome = 'Logroño',
+    ds_segundo_sobrenome = 'Trujillo',
+    ds_primeiro_nome = 'Emill',
+    ds_segundo_nome = 'Jossue',
+    nm_paciente = 'Emill Jossue Logroño Trujillo',
+    nm_social_paciente = 'Emill Jossue Logroño Trujillo'
+where
+    cd_paciente = 982961
+    
+  
+
+update paciente 
+set
+    ds_primeiro_sobrenome = 'Logroño',
+    ds_segundo_sobrenome = 'Trujillo',
+    ds_primeiro_nome = 'Emill',
+    ds_segundo_nome = 'Jossue',
+    nm_paciente = 'Emill Jossue Logroño Trujillo',
+    nm_social_paciente = 'Emill Jossue Logroño Trujillo'
+where
+    cd_paciente = 982961
+    
+    
+select * from paciente where paciente.cd_paciente = 982961
+
+
 select
     paciente.nm_paciente nm_paciente,
     paciente.cd_identificador_pessoa Cd_identificador_pessoa,
@@ -6,24 +43,30 @@ select
     paciente.ds_primeiro_nome ds_primeiro_nome,
     paciente.ds_segundo_nome ds_segundo_nome,
     paciente.cd_paciente cd_paciente,
-    atendime.cd_atendimento cd_atendimento
+    atendime.cd_atendimento cd_atendimento,
+    paciente.nm_social_paciente nm_social_paciente
 from
     paciente
     inner join atendime on paciente.cd_paciente = atendime.cd_paciente
 where
     atendime.cd_atendimento = 93217
+    
+    
+select * from paciente where paciente.cd_identificador_pessoa = '0604312801'
+select * from paciente where paciente.cd_identificador_pessoa = '0602268500'
+select * from paciente where paciente.cd_identificador_pessoa = '0201379492'
 
 
+/* {
+  "nm_paciente": "Emill Jossue Logroño Trujillo",
+  "ds_primeiro_sobrenome": "Logroño",
+  "ds_segundo_sobrenome": "Trujillo",
+  "ds_primeiro_nome": "Emill",
+  "ds_segundo_nome": "Jossue",
+  "nm_social_paciente": "Emill Jossue Logroño Trujillo"
+} */
 
-/* Hacer la actualización de paciente con inner join de atendimiento de nombre, cdecula, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido */
 
-update paciente 
-set
-    cd_identificador_pessoa = '0604312801',
-    ds_primeiro_sobrenome = 'Logroño',
-    ds_segundo_sobrenome = 'Trujillo',
-    ds_primeiro_nome = 'Emill',
-    ds_segundo_nome = 'Jossue',
-    nm_paciente = 'Emill Jossue Logroño Trujillo'
-where
-    cd_paciente = 982961
+-- 982961
+
+select * from hmetro.PACIENTE_AUD$ where cd_paciente_old = 982961

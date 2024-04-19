a95c5ab44635cfade02cb6941f6e2df9e57f9412
Lemito66 <lemito66@gmail.com>
2024-04-19 16:54:45 -0500

    sentencias para saber cuales son nulas, sin ruc y sin cédula

--- PLSQL/formularios/proveedores/index.sql
+++ PLSQL/formularios/proveedores/index.sql
@@ -2,6 +2,17 @@ select * from hm_itg_contactos_prov
 
 select * from hm_itg_proveedores where id = 176
 
+select * from hm_itg_proveedores proveedor where proveedor.password is not null 
+
+-- Proveedores con ruc vacio
+select * from hm_itg_proveedores proveedor where proveedor.ruc is  null 
+
+-- Proveedores con cedula vacia
+select * from hm_itg_proveedores proveedor where proveedor.cedula is null
+
+-- Proveedores con cedula y sin ruc
+select * from hm_itg_proveedores proveedor where proveedor.ruc is null and proveedor.cedula is not null
+
 select * from pais
 
 select * from uf
